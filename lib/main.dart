1948312fc10063e785b5465a9b8a2c15a59fd7e1
Luis Fernando Mendez Barrera <luis.fernando.mendez.barrera2708@gmail.com>
2023-08-22 18:08:41 -0600

    creación de aplicación

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+
+import 'package:adv_basics/quiz.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  //Define la función
+  runApp(
+   const Quiz()
+  ); //llama a la función y maetrialApp es el widget creado mandado a llamar
+}
+
