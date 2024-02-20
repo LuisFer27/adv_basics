1fd78bb948c1e7aba083d1c88e1ca85105fd001b
luis <luisfernando.anemona@gmail.com>
2024-02-19 18:25:40 -0600

    ajustes

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,9 +1,11 @@
 import 'package:adv_basics/answer_button.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+  final void Function(String answer) onSelectAnswer;
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
@@ -12,14 +14,15 @@ class QuestionsScreen extends StatefulWidget {
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
   var currentQuestionIndex = 0;
-  void answerQuestion(){
+  void answerQuestion(String selectedAnswer) {
+    widget.onSelectAnswer(selectedAnswer);
     //currentQuestionIndex = currentQuestionIndex + 1;
     //currentQuestionIndex+=1;
     setState(() {
-    currentQuestionIndex++;
+      currentQuestionIndex++;
     });
-
   }
+
   @override
   Widget build(context) {
     //return const Text('QuestionsScreen');
@@ -34,16 +37,24 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(
+                  color: const Color.fromARGB(255, 201, 153, 251),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(
               height: 30,
             ),
-            ...currentQuestion.getShuffledAnswers().map((answer){
-              return AnswerButton(answerText: answer, onTap: answerQuestion,);
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+                  answerQuestion(answer);
+                },
+              );
             })
-           /* AnswerButton(
+            /* AnswerButton(
               answerText: currentQuestion.answers[0],
               onTap: () {},
             ),
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,51 +1,68 @@
 import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
-
+import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/results_screen.dart';
 
-
-class Quiz extends StatefulWidget{
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
   @override
-  State<Quiz> createState(){
+  State<Quiz> createState() {
     return _QuizState();
   }
 }
 
-class _QuizState extends State<Quiz>{
-    var activeScreen='start-screen';
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+  List<String> selectedAnswers = [];
 
-void switchScreen(){
-setState(() {
-  activeScreen='questions-screen';
-});
-}
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        selectedAnswers = [];
+        activeScreen = 'results-screen';
+      });
+    }
+  }
 
   @override
-  Widget build(context){
+  Widget build(context) {
     /*final screenWidget=activeScreen=='start-screen'
            ? StartScreen(switchScreen)
            :const  QuestionsScreen();*/
-           Widget screenWidget=StartScreen(switchScreen);
-           if(activeScreen=='questions-screen'){
-           screenWidget=QuestionsScreen();
-           }
-
+    Widget screenWidget = StartScreen(switchScreen);
+    if (activeScreen == 'questions-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    }
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
+    }
     return MaterialApp(
       home: Scaffold(
-        body:Container(
-          decoration: const BoxDecoration(gradient:LinearGradient(
-            colors: [
-              Color.fromARGB(255, 78, 13, 151),
-              Color.fromARGB(255, 107, 15, 168),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
               ],
-              begin:Alignment.topLeft,
-              end:Alignment.bottomRight,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
           ),
           //ternario
-          child:screenWidget ,
-
+          child: screenWidget,
         ),
       ),
     );
