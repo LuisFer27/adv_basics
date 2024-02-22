14bcbcfaba3e1bc3ce40c469bc4cfd4672d2b708
luis <luisfernando.anemona@gmail.com>
2024-02-22 17:05:02 -0600

    últimos cambios

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,11 +1,13 @@
-import 'package:adv_basics/questions_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:adv_basics/data/questions.dart';
+
 import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/results_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
+
   @override
   State<Quiz> createState() {
     return _QuizState();
@@ -13,8 +15,8 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  var activeScreen = 'start-screen';
   List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
@@ -24,30 +26,38 @@ class _QuizState extends State<Quiz> {
 
   void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        //selectedAnswers = [];
         activeScreen = 'results-screen';
       });
     }
   }
 
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'questions-screen';
+    });
+  }
+
   @override
   Widget build(context) {
-    /*final screenWidget=activeScreen=='start-screen'
-           ? StartScreen(switchScreen)
-           :const  QuestionsScreen();*/
     Widget screenWidget = StartScreen(switchScreen);
+
     if (activeScreen == 'questions-screen') {
       screenWidget = QuestionsScreen(
         onSelectAnswer: chooseAnswer,
       );
     }
+
     if (activeScreen == 'results-screen') {
       screenWidget = ResultsScreen(
         chosenAnswers: selectedAnswers,
+        onRestart: restartQuiz,
       );
     }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -61,7 +71,6 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          //ternario
           child: screenWidget,
         ),
       ),
