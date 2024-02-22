99eb138415017cffc827fc86ed434165491227c7
luis <luisfernando.anemona@gmail.com>
2024-02-21 18:21:36 -0600

    cambios aplicación

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,11 +1,35 @@
+import 'package:adv_basics/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/questions_summary/questions_summary.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      //loop body
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData
+        .where((data) => data['user_answer'] == data['correct_answer'])
+        .length;
     return SizedBox(
         width: double.infinity,
         child: Container(
@@ -13,11 +37,13 @@ class ResultsScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text('You answered X out of Y questions correctly!'),
+                Text(
+                    'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!'),
                 const SizedBox(
                   height: 30,
                 ),
-                const Text('List of answers and questions...'),
+                //const Text('List of answers and questions...'),
+                QuestionsSummary(summaryData),
                 const SizedBox(
                   height: 30,
                 ),
